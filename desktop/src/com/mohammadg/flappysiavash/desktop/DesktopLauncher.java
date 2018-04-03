2473684c995955be1376019692fa48ce47ad40bf
Mohammad Ghasembeigi <me@mohammadg.com>
2018-04-03 17:13:36 +1000

    Initial commit (post libGDX setup)

---
+++ desktop/src/com/mohammadg/flappysiavash/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mohammadg.flappysiavash.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mohammadg.flappysiavash.FlappySiavashGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappySiavashGame(), config);
+	}
+}
---
+++ ios/src/com/mohammadg/flappysiavash/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.mohammadg.flappysiavash;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.mohammadg.flappysiavash.FlappySiavashGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new FlappySiavashGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
